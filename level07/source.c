5bf8febe3d54ab6fc68e3e6493d1d871feb0adee
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-06-01 14:31:28 +0200

    add write up

---
+++ level07/source.c
@@ -0,0 +1,28 @@
+
+
+
+int read_number() {
+
+  printf();
+  get_unum();
+  printf();
+
+}
+
+
+int store_number()
+{
+  printf();
+  get_unum();
+  printf();
+  get_unum();
+
+  puts();
+  puts();
+  puts();
+
+}
+
+int main() {
+
+}
