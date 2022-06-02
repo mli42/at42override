5a9a9bb71c01ed6da9ba4133179e65466c360a8d
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-06-02 16:39:28 +0200

    begin of level9

--- level09/source.c
+++ level09/source.c
@@ -12,13 +12,19 @@ int set_msg() {
 
 int handle_message() {
 
+  set_username();
+  set_msg();
+
 
 }
 
 
 int main() {
 
-  puts();
+  puts("
+      --------------------------------------------
+      |   ~Welcome to l33t-m$n ~    v1337        |
+      --------------------------------------------
+      ");
   handle_message();
-
 }
