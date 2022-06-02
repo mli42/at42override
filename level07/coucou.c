4db0f527ca6a67a0c3f255a8bf27efc585c79605
Baptiste Fraikin <fraikin.baptiste@gmail.com>
2022-06-02 12:36:21 +0200

    coucou bruteforce

---
+++ level07/coucou.c
@@ -0,0 +1,22 @@
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+
+int main() {
+
+  uint64_t to_test = 0x1;
+  unsigned int to_check;
+
+  while(to_test != 0xffffffff)
+  {
+    while (to_test % 3 == 0 || ((unsigned int)to_test) << 2 != 114 )
+      to_test++;
+    to_check = to_test;
+    printf("%" PRIx64 " %u %u\n", to_test, to_check, to_test % 3); 
+    to_test++;
+  }
+  printf("%p %u %d\n", to_test, to_check, to_test % 3); 
+}
