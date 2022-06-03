5960aef1fd484f2a3b9df7a8d368467a44557cc7
mli42 <mli@student.42.fr>
2022-06-03 03:36:22 +0200

    level09: source

--- level09/source.c
+++ level09/source.c
@@ -1,57 +1,55 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void secret_backdoor(void) {
+  char cmd[0x80];
 
-int secret_backdoor() {
-
-  char *cmd = fgets(stdin);
+  fgets(cmd, 0x80, stdin);
   system(cmd);
 }
 
+void set_username(char *buffer) {
+  char username[0x90 - 0x04]; // == 140
+  int i;
 
-int set_username() {
+  bzero(username, 0x10 * 8); // == 128
+  puts(">: Enter your username");
+  printf(">>: ");
+  fgets(username, 0x80, stdin);
 
-  char *username = fgets(stdin);
-
-  int i = 0;
-  while(username[i] != '\0')
+  i = 0;
+  char *buffer_username = &buffer[0x8c]; // Actually not on the stack
+  while(i <= 0x28 || username[i] != '\0')
   {
-    if (i > 0x28)
-    {
-      break;
+    buffer_username[i] = username[i];
     i++;
-    }
   }
-  printf("welcome %s", username);
+  printf(">: Welcome, %s", buffer_username);
 }
 
-int set_msg() {
-  
-  int index = 0x128;
-  while(index)
-    index--;
+void set_msg(char *buffer) {
+  char password[0x400];
 
+  bzero(password, 0x80 * 8); // == 0x400 == 1024
   puts(">: Msg @Unix-Dude");
-  printf("\n");
-  char *msg= fgets(stdin);
-  strncpy(msg)
+  printf(">>: ");
+  fgets(password, 0x400, stdin);
+  strncpy(buffer, password, buffer[0xb4]);
 }
 
+void handle_message(void) {
+  char buffer[0xc0 - 0xc];
+  int a = 0x8c;
 
-int handle_message() {
-
-  set_username();
-  set_msg();
+  bzero(&buffer[0x8c], 0x28);
+  set_username(buffer);
+  set_msg(buffer);
 
   puts(">: Msg sent!\n");
-
 }
 
-
-int main() {
-
-  puts("
-      --------------------------------------------
-      |   ~Welcome to l33t-m$n ~    v1337        |
-      --------------------------------------------
-      ");
+int main(void) {
+  puts("--------------------------------------------\n|   ~Welcome to l33t-m$n ~    v1337        |\n--------------------------------------------");
   handle_message();
 }
